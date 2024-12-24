36c1f2eae146d2063be1512101f1f4687cc06a62
Andika Auli <dikaauli25@gmail.com>
2024-12-24 16:44:21 +0700

    homePage

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 import RegisterScreen from './screen/register_screen';
+import HomeScreen from './screen/home';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(HomeScreen);
